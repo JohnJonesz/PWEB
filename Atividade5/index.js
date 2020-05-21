1e3d0f2a7c7eaef66f2b51c8ae8f6b85e6c30ddd
JRaaz <jonnyramos10@gmail.com>
2020-05-20 22:44:47 -0300

    Atividade 5

---
+++ Atividade5/index.js
@@ -0,0 +1,15 @@
+
+var numero1 = prompt('Insira o primeiro número:');
+var numero2 = prompt('Insira o segundo número:');
+var numero3 = prompt('Insira o terceiro número:');
+
+
+maiorNumero(numero1, numero2, numero3);
+
+function maiorNumero(numero1, numero2, numero3) {
+  var arrayNumeros = [numero1, numero2, numero3];
+
+  arrayNumeros.sort((a, b) => a - b);
+
+  alert("O maior número é o " + arrayNumeros[arrayNumeros.length - 1]);
+}
