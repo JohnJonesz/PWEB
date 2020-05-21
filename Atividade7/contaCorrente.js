559d00a9ba45e6ca846eb2c56d0a36b3a7d9a28a
JRaaz <jonnyramos10@gmail.com>
2020-05-20 22:59:45 -0300

    Atividade 7

---
+++ Atividade7/contaCorrente.js
@@ -0,0 +1,12 @@
+
+function Corrente() {
+  this.getSaldoEspecial = () => {
+    return this.saldoEspecial;
+  };
+
+  this.setSaldoEspecial = (saldoEspecial) => {
+    this.saldoEspecial = saldoEspecial;
+  };
+}
+
+Corrente.prototype = new Conta();
