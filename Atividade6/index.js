9e3919d07aa50c51d02c20f9693140fab58c28f1
JRaaz <jonnyramos10@gmail.com>
2020-05-20 22:52:20 -0300

    Atividade 6

---
+++ Atividade6/index.js
@@ -0,0 +1,14 @@
+
+var Aluno1 = {
+  ra: '0030481810000',
+  nome: 'Joao'
+}
+console.log(Aluno1);
+
+Aluno1.ra = '0030481810000';
+Aluno1.nome = 'Jose';
+console.log(Aluno1);
+
+Aluno1['ra'] = '0030481810000';
+Aluno1['nome'] = 'Marcos';
+console.log(Aluno1);
